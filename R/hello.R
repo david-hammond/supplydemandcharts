25be1c137d7caeb8d9d9f6ec4b6edbb3dd507f73
david-hammond <dhammond@economicsandpeace.org>
2017-07-27 15:59:32 +1000

    tests

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,6 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
-hello <- function() {
-  print("Hello, world!")
-}
+supply.demand()
+supply.demand.consumer.surplus()
+shift.demand()
+shift.demand("left")
+shift.supply()
+shift.supply("left")
